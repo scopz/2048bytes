199827a87ede401ea8fd500164aeaeb4dbfa7f64
scopp <osc.car2@gmail.com>
2024-08-10 21:20:32 +0200

    Remove custom animations

--- app/src/main/java/org/oar/bytes/utils/ComponentsExt.kt
+++ app/src/main/java/org/oar/bytes/utils/ComponentsExt.kt
@@ -8,12 +8,4 @@ import org.oar.bytes.R
 object ComponentsExt {
     fun Fragment.runOnUiThread(action: Runnable) = requireActivity().runOnUiThread(action)
     fun View.runOnUiThread(action: Runnable) = (context as Activity).runOnUiThread(action)
-
-    fun Activity.overridePendingSideTransition(toLeft: Boolean = true) {
-        if (toLeft) {
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        } else {
-            overridePendingTransition(R.anim.slide_in_left_back, R.anim.slide_out_right_back)
-        }
-    }
 }
